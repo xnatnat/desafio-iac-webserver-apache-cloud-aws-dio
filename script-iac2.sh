8ccd073033f9056eba09bb1f29072bfec3e32415
Nathalia Mota <74469689+xnatnat@users.noreply.github.com>
2023-05-16 22:17:09 -0300

    Add files via upload

---
+++ script-iac2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "<>--- Iniciando Script ---<>"
+
+echo "<>--- Atualizando o servidor ---<>"
+
+apt_get update
+apt_get upgrade
+
+echo "<>--- Realizando instalacoes necessarias ---<>"
+
+apt_get install apache2 unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+cd linux-site-dio-main
+cp-R * /var/www/html/
+
+echo "<>--- Finalizado ---<>"
